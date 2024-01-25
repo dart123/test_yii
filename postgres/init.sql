d5222de68558c89391fa27c827540cec890c64b9
Aleksandr Zemyanskiy <aleksandr.zemyanskiy@microbiz.com>
2024-01-25 18:14:13 +0400

    added user and db creation to init.sql

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,3 +1,5 @@
+CREATE USER test_user;
+CREATE DATABASE test_yii
 GRANT ALL PRIVILEGES ON DATABASE test_yii TO test_user;
 CREATE TYPE decision_type AS ENUM ('approved', 'declined');
 CREATE TABLE images(
