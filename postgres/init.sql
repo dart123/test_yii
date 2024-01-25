0d278778eefadbf705583a96838b724d5b501983
Aleksandr Zemyanskiy <aleksandr.zemyanskiy@microbiz.com>
2024-01-25 18:06:24 +0400

    in app DockerFile changed working dir, added postgres Dockerfile and moved env variables there from docker-compose.yml, added table creation to init.sql

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,3 +1,7 @@
-CREATE USER test_user;
-CREATE DATABASE test_yii
 GRANT ALL PRIVILEGES ON DATABASE test_yii TO test_user;
+CREATE TYPE decision_type AS ENUM ('approved', 'declined');
+CREATE TABLE images(
+  image_id PRIMARY KEY, 
+  link VARCHAR (255) UNIQUE NOT NULL, 
+  decision decision_type NOT NULL
+);
